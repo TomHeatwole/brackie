4a8dfc45b8f4977c40ab321e55cc7c5ede75ff59
tomheatwole <tgh8xn@virginia.edu>
2026-03-19 16:54:32 -0400

    fix

--- app/pools/[id]/_components/pool-tabs.tsx
+++ app/pools/[id]/_components/pool-tabs.tsx
@@ -698,7 +698,7 @@ export default function PoolTabs({
                                 )}
                               </div>
                             </div>
-                            <div className="w-full md:w-auto md:flex-1 md:justify-center overflow-x-auto overflow-y-hidden">
+                            <div className="w-full md:w-auto md:flex-1 md:justify-center">
                               <div className="grid grid-cols-7 gap-2 px-0 md:px-4 min-w-[460px] md:min-w-0">
                                 <div className="flex flex-col items-center">
                                   <span className="text-[11px] text-stone-500">Total</span>
